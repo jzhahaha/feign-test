8d9ca73ad386a99f2d6689d38d11b92ef948e229
zoujun <junzou214916@sohu-inc.com>
2018-09-12 21:52:10 +0800

    feign

--- src/main/java/com/zjtest/springcloud/controller/FeignTest.java
+++ src/main/java/com/zjtest/springcloud/controller/FeignTest.java
@@ -1,4 +1,30 @@
 package com.zjtest.springcloud.controller;
 
+import com.zjtest.springcloud.service.client.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
 public class FeignTest {
+
+    @Resource
+    private UserService userService;
+
+    @GetMapping("/feign-consumer")
+    public ResponseEntity<?> getModel(@RequestParam("id") int id){
+
+        return new ResponseEntity<Map>(userService.getModel(id), OK);
+
+
+    }
+
+
 }
--- src/main/java/com/zjtest/springcloud/service/client/UserService.java
+++ src/main/java/com/zjtest/springcloud/service/client/UserService.java
@@ -1,4 +1,15 @@
 package com.zjtest.springcloud.service.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@FeignClient(value = "test-everything", url = "localhost:6789/")
 public interface UserService {
+
+    @GetMapping("/test1/getModel")
+    Map<String, String> getModel(@RequestParam("id")int id);
+
 }
